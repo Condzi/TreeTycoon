bc05b7d44c573edc1471d3ef15a2fca87f030de0
Condzi <Condzi@users.noreply.github.com>
2018-04-18 19:21:57 +0200

    add Texture unit tests

--- TreeTycoon/UnitTests/Src/Assets.cpp
+++ TreeTycoon/UnitTests/Src/Assets.cpp
@@ -12,6 +12,8 @@
 
 TEST_CASE( "INI File Reader", "[Assets]" )
 {
+	static constexpr const char* TEST_INI_PATH = "test.ini";
+
 	{
 		con::INIReader ini;
 		ini.setValue( "SECTION_A", "name1", "val1" );
@@ -20,12 +22,12 @@ TEST_CASE( "INI File Reader", "[Assets]" )
 		ini.setValue( "SECTION_B", "name1", "val1" );
 		ini.setValue( "SECTION_B", "name2", "val2" );
 
-		ini.save( "test.ini" );
+		ini.save( TEST_INI_PATH );
 	}
 
 	{
 		con::INIReader ini;
-		REQUIRE( ini.load( "test.ini" ) == true );
+		REQUIRE( ini.load( TEST_INI_PATH ) == true );
 		REQUIRE( ini.getValue( "SECTION_A", "name1" ).has_value() );
 		REQUIRE( ini.getValue( "SECTION_A", "name1" ).value_or( "(err)" ) == "val1" );
 		REQUIRE( ini.getValue( "SECTION_A", "name2" ).has_value() );
@@ -36,5 +38,27 @@ TEST_CASE( "INI File Reader", "[Assets]" )
 		REQUIRE( ini.getValue( "SECTION_B", "name2" ).has_value() );
 		REQUIRE( ini.getValue( "SECTION_B", "name2" ).value_or( "(err)" ) == "val2" );
 	}
-	std::experimental::filesystem::remove( "test.ini" );
+
+	std::experimental::filesystem::remove( TEST_INI_PATH );
 }
+
+
+TEST_CASE( "Basic Asset Holder (sf::Texture)", "[Assets]" )
+{
+	static constexpr const char* TEST_IMG_PATH = "test.png";
+
+	{
+		sf::Image testImg;
+		testImg.create( 32, 32, sf::Color::Green );
+		testImg.saveToFile( TEST_IMG_PATH );
+	}
+
+	{
+		REQUIRE( con::Assets.Texture.load( TEST_IMG_PATH, "test" ) == true );
+		// green vs red
+		REQUIRE( con::Assets.Texture.get( "test" ).copyToImage().getPixel( 0, 0 ) != con::Assets.Texture.getDefault().copyToImage().getPixel( 0, 0 ) );
+	}
+
+	std::experimental::filesystem::remove( TEST_IMG_PATH );
+}
+
