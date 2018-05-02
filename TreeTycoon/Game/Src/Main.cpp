84ac3521f7343a443ef1b4f3c68cc8c62d563cee
Condzi <Condzi@users.noreply.github.com>
2018-05-02 21:55:21 +0200

    clean up Game's project int main

--- TreeTycoon/Game/Src/Main.cpp
+++ TreeTycoon/Game/Src/Main.cpp
@@ -3,33 +3,5 @@
 	https://github.com/condzi
 */
 
-#include <Debug.hpp>
-
-using namespace con;
-
 int main()
-{
-	class Logger : public ILogger
-	{
-	private:
-		std::string loggerName() const override
-		{
-			return ConvertTo<std::string>( 123 );
-		}
-	public:
-		void test()
-		{
-			log( LogPriority::Info, "info." );
-			log( LogPriority::Warning, "warning." );
-			log( LogPriority::Error, "error." );
-			debugLog( LogPriority::Info, "debug info." );
-			debugLog( LogPriority::Warning, "debug warning." );
-			debugLog( LogPriority::Error, "debug error." );
-		}
-	};
-
-	Logger l;
-	l.test();
-
-	std::cin.get();
-}
+{}
