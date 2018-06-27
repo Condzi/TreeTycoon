66bd047686c10fbca5014d87a2606a6c187d2f60
Condzi <Condzi@users.noreply.github.com>
2018-06-27 12:31:35 +0200

    remove try-catch from Main - disturbs in Debug

--- TreeTycoon/Game/Src/Main.cpp
+++ TreeTycoon/Game/Src/Main.cpp
@@ -5,14 +5,9 @@
 
 int main()
 {
-	try {
-		con::Global.SceneStack.registerScene<WorldScene>( 0 );
-		con::Global.SceneStack.registerScene<PlotViewScene>( 1 );
-		con::Global.SceneStack.push( 0 );
+	con::Global.SceneStack.registerScene<WorldScene>( 0 );
+	con::Global.SceneStack.registerScene<PlotViewScene>( 1 );
+	con::Global.SceneStack.push( 0 );
 
-		con::Global.Game.run();
-	} catch ( const std::exception& exception ) {
-		std::cout << std::endl << exception.what();
-		std::cin.get();
-	}
+	con::Global.Game.run();
 }
