9489fecd331a85034fc29592f147f1003f37fc14
Condzi <Condzi@users.noreply.github.com>
2018-06-28 23:03:03 +0200

    spawn trees only once, better switch between world and plot view

--- TreeTycoon/Game/Inc/PlotViewScene.hpp
+++ TreeTycoon/Game/Inc/PlotViewScene.hpp
@@ -20,16 +20,20 @@ public:
 		tag = con::ConvertTo<std::string>( "PlotView(", plot->getInfo().name, ")" );
 		addSystem<PlotRenderingSystem>( 0 );
 
-		for ( size_t y = 0; y < 12; y++ )
-			for ( size_t x = 0; x < 12; x++ ) {
-				plot->spawnTree( std::hash<std::string>{}( "Apple" ), { x,y } ).value()->stats.growingState = con::Random( 0, 2 );
-			}
+		if ( static bool spawned = false; !spawned ) {
+			spawned = true;
+			for ( size_t y = 0; y < 12; y++ )
+				for ( size_t x = 0; x < 12; x++ ) {
+					plot->spawnTree( std::hash<std::string>{}( "Apple" ), { x,y } ).value()->stats.growingState = con::Random( 0, 2 );
+				}
+		} 
 	}
 
 private:
 	void loadGui()
 	{
 		auto& gui = con::Global.GUI;
+		gui.removeAllWidgets();
 		gui.loadWidgetsFromFile( constants::PLOT_VIEW_GUI_PATH );
 
 		gui.get( "button_exit" )->connect( "pressed", []() {
@@ -37,9 +41,7 @@ private:
 		} );
 
 		gui.get( "button_world" )->connect( "pressed", []() {
-			auto& sceneStack = con::Global.SceneStack;
-			sceneStack.pop();
-			sceneStack.enableCurrentScene();
+			 con::Global.SceneStack.pop();
 		} );
 	}
 };
--- TreeTycoon/Game/Inc/WorldScene.hpp
+++ TreeTycoon/Game/Inc/WorldScene.hpp
@@ -17,18 +17,23 @@ public:
 		tag = "World";
 		GlobalGameData.TreeInfos.load();
 		GlobalGameData.Plots.load( *this );
-		loadGui();
 	}
 
-	void onEnable() override
+	void onUpdate() override
 	{
-		loadGui();
+		static Scene* lastScene = nullptr;
+		if ( con::Global.SceneStack.getSceneOnTop().value_or( nullptr ) == this && lastScene != this ) {
+			lastScene = this;
+			loadGui();
+		} else
+			lastScene = con::Global.SceneStack.getSceneOnTop().value_or( nullptr );
 	}
 
 private:
 	void loadGui()
 	{
 		auto& gui = con::Global.GUI;
+		gui.removeAllWidgets();
 		gui.loadWidgetsFromFile( constants::WORLD_VIEW_GUI_PATH );
 
 		gui.get( "button_exit" )->connect( "pressed", []() {
@@ -44,7 +49,6 @@ private:
 		}
 
 		plotsList.connect( "itemSelected", [&]( const sf::String& itemId ) {
-			con::Global.SceneStack.disableCurrentScene();
 			con::Global.SceneStack.push( 1 );
 			GlobalGameData.CurrentPlot = GlobalGameData.Plots.findPlot( itemId ).value_or( nullptr );
 		} );
